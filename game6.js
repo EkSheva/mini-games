71d5eabca20dcc8676d0ad9f2f206c58c597f838
EkSheva <katufka13@mail.ru>
2025-02-27 12:18:32 +0300

    Correct id

--- game6.js
+++ game6.js
@@ -1,6 +1,6 @@
 function game6() {
     
-    const btn = document.getElementById('game6');
+    const btn = document.getElementById('gameNumber6');
     const fon = document.querySelector('.main');
     const randomHexColor=`#${Math.floor(Math.random() * 16777215).toString(16)}`;
     btn.addEventListener('click', () => {
